17e10ee2a4c24fb6a4e335a80db3f84a9669a0b9
Kazuya Igari <k.igari1013@gmail.com>
2017-10-30 01:17:17 +0900

    :sparkles: update to createWebhook()

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 function createWebhook(){
-    // stub
+    var scriptProp =  PropertiesService.getScriptProperties().getProperties();
+    var trelloKey = scriptProp.TRELLO_API_KEY;
+    var trelloToken = scriptProp.TRELLO_API_TOKEN;
+    var callbackURL = scriptProp.CALLBACK_URL;
+    var boardId = scriptProp.BOARD_ID;
+
+    var url = 'https://api.trello.com/1/tokens/' + trelloToken + '/webhooks/?key=' + trelloKey;
+    var options = {
+        'method' : 'post',
+        'payload' : {
+            'description': 'Trello to Line',
+            'callbackURL': callbackURL,
+            'idModel': boardId
+        }
+    }
+    Logger.log(UrlFetchApp.fetch(url, options));
 }
 
 function postLineMessage(message){
